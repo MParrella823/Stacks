3f54b90dad3cdeb0743834c8169e8769e1139d98
Matt Parrella <parrellam617@strose.edu>
2016-10-14 18:18:57 -0400

    Add files via upload

--- PostCalc.java
+++ PostCalc.java
@@ -15,89 +15,89 @@ public class PostCalc {
 				
 				int answer = 0;
 				
-				/*
-				 * The following loop will traverse the string, storing the value at each index
-				 * as a character variable.  It will then check if that variable is a digit or an operand.
-				 * 
-				 * If it's a digit, it will add that number to the stack and continue on in this manner
-				 * until it finds an operand.  It will then perform that arithmatic on the two numbers in 
-				 * in the stack and push the answer onto the stack.
-				 * 
-				 */
-			   			
-				for (int i = 0; i < s.length(); i ++){  
-					int a = 0;
-					int b = 0;
-					int d = 0;
-					char c = s.charAt(i);  // Variable to hold the character value at each index
-					
-					if (Character.isDigit(c) == true){  
-						int x = Character.getNumericValue(c); // If yes, get the numerical value (otherwise ASCII value is used)
-						s1.push(x); 
-					}
-					
+				if (s.equals("quit")){
+					kb.close();
+					input.close();
+					break;
+				}
+				
+				else{
+				
 					/*
-					 * The remaining conditionals will check for operands in the string.
-					 * If found, it will perform the operation on the previous two numbers
-					 * and add the result to the stack
+					 * The following loop will traverse the string, storing the value at each index
+					 * as a character variable.  It will then check if that variable is a digit or an operand.
+					 * 
+					 * If it's a digit, it will add that number to the stack and continue on in this manner
+					 * until it finds an operand.  It will then perform that arithmatic on the two numbers in 
+					 * in the stack and push the answer onto the stack.
 					 * 
 					 */
-					
-					else if (c == '+'){ 
-						a = s1.pop();
-						b = s1.pop();
-						d = b + a;
-						
-						s1.push(d);
-					}
-					
-					else if (c == '-'){
-						a = s1.pop();
-						b = s1.pop();
-						d = b - a;
-						
-						s1.push(d);
-					}
-					
-					else if (c == '*'){
-						a = s1.pop();
-						b = s1.pop();
-						d = b * a;
-						
-						s1.push(d);
-					}
-					
-					else if (c == '/'){
-						a = s1.pop();
-						b = s1.pop();
-						d = b / a;
-						
-						s1.push(d);
-					}					
-				}				
+
+					for (int i = 0; i < s.length(); i ++){  
+						int a = 0;
+						int b = 0;
+						int d = 0;
+						char c = s.charAt(i);  // Variable to hold the character value at each index
+
+						if (Character.isDigit(c) == true){  
+							int x = Character.getNumericValue(c); // If yes, get the numerical value (otherwise ASCII value is used)
+							s1.push(x); 
+						}
+
+						/*
+						 * The remaining conditionals will check for operands in the string.
+						 * If found, it will perform the operation on the previous two numbers
+						 * and add the result to the stack
+						 * 
+						 */
+
+						else if (c == '+'){ 
+							a = s1.pop();
+							b = s1.pop();
+							d = b + a;
+
+							s1.push(d);
+						}
+
+						else if (c == '-'){
+							a = s1.pop();
+							b = s1.pop();
+							d = b - a;
+
+							s1.push(d);
+						}
+
+						else if (c == '*'){
+							a = s1.pop();
+							b = s1.pop();
+							d = b * a;
+
+							s1.push(d);
+						}
+
+						else if (c == '/'){
+							a = s1.pop();
+							b = s1.pop();
+							
+							if (a == 0){
+								
+								System.out.println("Error: Division by zero!");
+								
+							}
+							
+							else{
+								d = b / a;
+								s1.push(d);
+							}
+						}					
+					}				
 				// The last item added to the stack will be the final answer
-			
-				answer = s1.pop();
+			 	answer = s1.pop();
 				System.out.println("Answer: " + answer);
+				}
 			}
 			
 			while (!s.equals("quit"));
-			
-			
-			System.out.println("Exiting the PostCalc program!");
-	
-			
-			
-			
-			
-			
+			System.out.println("Exiting the PostCalc program!");		
 		}
-			
-			
-			
-
-	
-		
-	
-	
 }
